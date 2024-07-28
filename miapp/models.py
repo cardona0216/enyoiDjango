7566fb9c5ded4a66090106da6f47e2763084b809
waltercardona <wacardona10@gmail.com>
2024-07-28 09:09:46 -0500

    se trabajo en el modelo para la cracion de las tablas y se trabajo en las vistas

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+# con esto creamos una table con el models.Model 
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+# con esto creamos una table con el models.Model 
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    last_update = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
+    starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+
+# con esto creamos una table con el models.Model 
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='posts',on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(null=True)
+    create_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    update_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
--- miapp/views.py
+++ miapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import  HttpResponse
+from .models import Board
 
-# Create your views here.
+
+
+def home(request):
+    boards = Board.objects.all()
+    boards_name = list()
+    for board in boards:
+        boards_name.append(board.name)
+
+    response_html = '<br>'.join(boards_name)
+    return HttpResponse(response_html)
